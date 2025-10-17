0b5263e245a2e26a3bf63311db86f2bbf26d3943
Muhammad Zafran IMSCIENCES <zafrankhaan33@gmail.com>
2025-10-17 18:31:29 +0500

    Add files via upload

---
+++ Linked List /Lab_06_Task/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Node{
+	public:
+		string data;
+		Node* prev;
+		Node* next;
+		
+		Node(string data){
+			this->data=data;
+			this->next=nullptr;
+			this->prev=nullptr;
+			
+		}
+};
+class DoublyLL{
+	public:
+		Node* head;
+		Node* Tail;
+		DoublyLL(){
+			head = nullptr;
+			Tail = nullptr;
+		}
+			
+	void insertAtBeginning(string value){
+			Node* newnode= new Node(value);
+			
+			if(head == nullptr){
+			head =newnode;
+			Tail = newnode;
+			}
+			else
+			{
+				newnode->next=head;
+				head->prev=newnode;
+				head=newnode;
+			}
+		}
+	        
+	void displayFront()
+	{
+		Node* temp=head;
+		if(temp == nullptr){
+			cout<<"the list is Empty";
+			return;
+		}
+	
+		while(temp != nullptr){
+			cout<<temp->data<<"  ";  
+			temp=temp->next;
+		}
+		cout<<endl;
+	}
+	        
+	void displayBack()
+	{
+		Node* temp=Tail;
+		
+		if(temp == nullptr){
+			cout<<"the list is Empty";
+			return;
+		}
+		
+	  
+		while(temp!= nullptr){
+			cout<<temp->data<<"   ";  
+			temp=temp->prev;
+		}
+		cout<<endl;
+	}
+};
+
+int main(int argc, char** argv) {
+	DoublyLL ll;
+
+	ifstream ip_file("ips.txt");
+
+	if (!ip_file.is_open()) {
+		cerr << "Error: Could not open the file 'ips.txt'. Make sure it's in the same directory." << endl;
+		return 1;
+	}
+
+	string line;
+	getline(ip_file, line);
+	ip_file.close();
+
+	stringstream ss(line);
+	string ip;
+
+	while (getline(ss, ip, ',')) {
+		ll.insertAtBeginning(ip);  
+	}
+	
+	cout<<endl;
+	
+	cout << "Forward traversal: \n"<<endl;
+	ll.displayFront();
+	
+	cout<<endl;
+	cout << "Backward traversal: \n"<<endl;
+	ll.displayBack();
+	
+	return 0;
+};
