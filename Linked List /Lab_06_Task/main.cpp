3d2959d758dc9f41e25b6c7d5c4e7758870b151f
Muhammad Zafran IMSCIENCES <zafrankhaan33@gmail.com>
2025-10-31 23:46:46 +0500

    Update main.cpp

--- Linked List /Lab_06_Task/main.cpp
+++ Linked List /Lab_06_Task/main.cpp
@@ -4,120 +4,113 @@
 #include <sstream>
 using namespace std;
 
-class Node{
-	public:
-		string data;
-		Node* prev;
-		Node* next;
-		
-		Node(string data){
-			this->data=data;
-			this->next=nullptr;
-			this->prev=nullptr;
-			
-		}
-};
-class DoublyLL{
-	public:
-		Node* head;
-		Node* Tail;
-		DoublyLL(){
-			head = nullptr;
-			Tail = nullptr;
-		}
-			
-	void insertAtBeginning(string value){
-			Node* newnode= new Node(value);
-			
-			if(head == nullptr){
-			head =newnode;
-			Tail = newnode;
-			}
-			else
-			{
-				newnode->next=head;
-				head->prev=newnode;
-				head=newnode;
-			}
-		}
-	        
-	void displayFront()
-	{
-		Node* temp=head;
-		if(temp == nullptr){
-			cout<<"the list is Empty";
-			return;
-		}
-	
-		while(temp != nullptr){
-			cout<<temp->data<<"  ";  
-			temp=temp->next;
-		}
-		cout<<endl;
-	}
-	        
-	void displayBack()
-	{
-		Node* temp=Tail;
-		
-		if(temp == nullptr){
-			cout<<"the list is Empty";
-			return;
-		}
-		
-	  
-		while(temp!= nullptr){
-			cout<<temp->data<<"   ";  
-			temp=temp->prev;
-		}
-		cout<<endl;
-	}
-	
-	void freeList() {
-		Node* current = head;
-		while (current != nullptr) {
-			Node* nodeToDelete = current; 
-			current = current->next;      
-			delete nodeToDelete;          
-		}
-		head = nullptr;  
-		Tail = nullptr;  
-		cout << "\nMemory freed successfully!" << endl;
-	}
+class Node {
+public:
+    string data;
+    Node* prev;
+    Node* next;
+    
+    Node(string d) {
+        data = d;
+        next = nullptr;
+        prev = nullptr;
+    }
 };
 
-int main(int argc, char** argv) {
-	DoublyLL ll;
+class DoublyLL {
+public:
+    Node* head;
+    Node* tail;  
+    
+    DoublyLL() {
+        head = nullptr;
+        tail = nullptr;
+    }
+    
+    void insertAtStart(string value) {  
+        Node* newNode = new Node(value);
+        
+        if(head == NULL) {  
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head;
+            if(head != NULL) {  
+                head->prev = newNode;
+            }
+            head = newNode;
+        }
+    }
+    
+    void showForward() {  
+        Node* temp = head;
+        if(!temp) {
+            cout << "List is empty!" << endl;  
+            return;
+        }
+        
+        while(temp != NULL) {  
+            cout << temp->data;
+            if(temp->next) cout << " -> ";  
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    
+    void showBackward() {
+        if(tail == nullptr) {
+            cout << "Nothing to show" << endl;  
+            return;
+        }
+        
+        Node* current = tail;
+        while(current) {
+            cout << current->data;
+            if(current->prev) cout << " <- ";
+            current = current->prev;
+        }
+        cout << endl;
+    }
+    
+    void cleanup() {
+        Node* current = head;
+        while(current) {
+            Node* toDelete = current;
+            current = current->next;
+            delete toDelete;
+        }
+        head = tail = nullptr;
+    }
+};
 
-	ifstream ip_file("ips.txt");
-
-	if (!ip_file.is_open()) {
-		cerr << "Error: Could not open the file 'ips.txt'. Make sure it's in the same directory." << endl;
-		return 1;
-	}
-
-	string line;
-	getline(ip_file, line);
-	ip_file.close();
-
-	stringstream ss(line);
-	string ip;
-
-	while (getline(ss, ip, ',')) {
-		ll.insertAtBeginning(ip);  
-	}
-	
-	cout<<endl;
-	
-	cout << "Forward traversal: \n"<<endl;
-	ll.displayFront();
-	
-	cout<<endl;
-	cout << "Backward traversal: \n"<<endl;
-	ll.displayBack();
-	
-	
-	ll.freeList();
-	
-	return 0;
+int main() {
+    DoublyLL list;  
+    
+    ifstream file("ips.txt");
+    if(!file) {  
+        cout << "Can't open file!" << endl;
+        return 1;
+    }
+    
+    string line;
+    getline(file, line);
+    file.close();
+    
+    stringstream ss(line);
+    string ip;
+    
+    
+    while(getline(ss, ip, ',')) {
+        list.insertAtStart(ip);
+    }
+    
+    cout << "Forward: " << endl;
+    list.showForward();
+    
+    cout << "\nBackward: " << endl;  
+    list.showBackward();
+    
+    list.cleanup();
+    
+    return 0;
 }
